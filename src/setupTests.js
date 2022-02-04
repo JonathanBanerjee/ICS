001a6fac86fbe439583a57b4f4108fb3ba32b859
jonathan banerjee <jonathan@youd.net>
2022-02-04 18:30:50 +0000

    Jest?

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+test('Move to login page', () => {
+    const mockLogin = jest.fn();
+    const wrapper = shallow(<Component startLogin={Login}/>);
+    wrapper.find('button').at(0).simulate('click');
+    expect({Login}).toHaveBeenCalled();
+});
